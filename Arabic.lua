efb3e474cadeefaac508540633c33971a6067029
PO2_Z-ON-TOP <108760881+nn0kia@users.noreply.github.com>
2023-06-27 02:29:22 +0300

    Update Arabic.lua

--- Arabic.lua
+++ Arabic.lua
@@ -2,7 +2,7 @@ game.StarterGui:SetCore("SendNotification",{Title = "كوكاين" ,Text = "لل
 -- اخذت سكربت النسخ من انفنتي ادمن و اركب عليها اذا براسك شي
 local copy = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set)
 if copy then
-copy("loadstring(game:HttpGet('https://raw.githubusercontent.com/Abdullah16661/C0cAiNe/main/RagdollEngine.lua'))()")
+copy("loadstring(game:HttpGet('https://raw.githubusercontent.com/Abdullah16661/C0cAiNe/main/launcher.lua'))()")
 game.StarterGui:SetCore("SendNotification",{Title = "كوكاين" ,Text = "الجديد السكربت نسخ تم" , Duration = 4, Icon = nil}) 
 else
 game.StarterGui:SetCore("SendNotification",{Title = "كوكاين" ,Text = النسخ مايدعم حقك الهاك" , Duration = 4, Icon = nil}) 
